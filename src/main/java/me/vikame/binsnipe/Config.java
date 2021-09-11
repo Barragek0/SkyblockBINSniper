6ee9358dd0d05ce35237121ae66cfe20f93b1514
Walter Scheveck <walterkillmenowpls@gmail.com>
2021-09-10 23:47:46 -0400

    Implement a configuration file to modify how the sniper behaves.

--- src/main/java/me/vikame/binsnipe/Config.java
+++ src/main/java/me/vikame/binsnipe/Config.java
@@ -5,7 +5,7 @@ public class Config {
   /* Whether to add the "Accept-Encoding: gzip" header to API requests.
    * I would highly recommend against disabling this as it greatly reduces bandwith usage when enabled.
    */
-  public static final boolean USE_GZIP_COMPRESSION_ON_API_REQUESTS = true;
+  public static boolean USE_GZIP_COMPRESSION_ON_API_REQUESTS = true;
 
   /* Whether to ensure all API requests do not return a cached response.
    * This employs many tactics to ensure that we do not get a cached response,
@@ -15,37 +15,37 @@ public class Config {
    * I would recommend keeping this disabled, but if you do not care about your bandwith usage you
    * *may* see a speed-up from enabling this setting.
    */
-  public static final boolean FORCE_NO_CACHE_API_REQUESTS = false;
+  public static boolean FORCE_NO_CACHE_API_REQUESTS = false;
 
   // Whether to ignore furniture items, as these are commonly market-manipulated.
-  public static final boolean IGNORE_FURNITURE = true;
+  public static boolean IGNORE_FURNITURE = true;
 
   // Whether to ignore cosmetic items, as these are commonly market-manipulated.
-  public static final boolean IGNORE_COSMETICS = true;
+  public static boolean IGNORE_COSMETICS = true;
 
   // Whether to treat all "dungeonizable" items as if they were starless.
-  public static final boolean IGNORE_STARS = false;
+  public static boolean IGNORE_STARS = false;
 
   // Whether to treat all reforgeable items the same regardless of applied reforge.
-  public static final boolean IGNORE_REFORGES = true;
+  public static boolean IGNORE_REFORGES = true;
 
   // Whether to ignore whether an item has hot potato books applied.
-  public static final boolean IGNORE_HOT_POTATO = true;
+  public static boolean IGNORE_HOT_POTATO = true;
 
   // The minimum number of the same item that must be on the market to be considered for a flip.
-  public static final int MIN_ITEMS_ON_MARKET = 3;
+  public static int MIN_ITEMS_ON_MARKET = 3;
 
   // The maximum price you are willing to pay for an item to flip.
-  public static final int MAX_BIN_PRICE = Integer.MAX_VALUE;
+  public static int MAX_BIN_PRICE = Integer.MAX_VALUE;
 
   // The minimum price the item to be flipped must be able to sell for.
-  public static final int MIN_BIN_PRICE = 1_000_000;
+  public static int MIN_BIN_PRICE = 1_000_000;
 
   // The amount of profit required to log a flip. When checking this, we check if *either* the percentage profit gain or raw profit amount are above their respective threshold.
-  public static final float MIN_PROFIT_PERCENTAGE = 110.0f;
-  public static final int MIN_PROFIT_AMOUNT = 1_000_000;
+  public static float MIN_PROFIT_PERCENTAGE = 110.0f;
+  public static int MIN_PROFIT_AMOUNT = 1_000_000;
 
   // The number of segments to a console-based loading bar used when displaying current BIN snipe progress.
-  public static final int LOADING_BAR_SEGMENTS = 40;
+  public static final transient int LOADING_BAR_SEGMENTS = 40;
 
 }
